33b3f352f6a47b27e96a706d5976643af836a604
Koen Verbruggen <Verbruggen.Koen@gmail.com>
2015-12-10 03:45:25 +0100

    cleanup

--- sysinfo_adv.py
+++ sysinfo_adv.py
@@ -99,14 +99,11 @@ def button_press(event):
 			cad.lcd.display_on()
 			cad.lcd.backlight_on()
 			BACKLIGHT = True
-		
-		# keep the status for at least INFO_TIME
-		sleep(INFO_TIME)
 	
 	# sysinfo
-	# backlight toggle
 	if event.pin_num == 0 :
 		show_sysinfo()
+
 
 # shutdown function
 def cmd_shutdown():
@@ -141,7 +138,7 @@ def start_routine():
 
 	listener.activate()
 
-# DEBUG function
+# BYE function
 def stop_routine():
 	listener.deactivate()
 
@@ -156,11 +153,11 @@ atexit.register(stop_routine)
 
 
 ## sysinfo - CMD / VAR
-#UPDATE_INTERVAL = 60 * 5  # 5 mins
 GET_IP_CMD = "hostname --all-ip-addresses"
 GET_TEMP_CMD = "/opt/vc/bin/vcgencmd measure_temp"
 TOTAL_MEM_CMD = "free | grep 'Mem' | awk '{print $2}'"
 USED_MEM_CMD = "free | grep '\-\/+' | awk '{print $3}'"
+
 
 ## sysinfo - custom symbols
 temperature_symbol = pifacecad.LCDBitmap(
@@ -168,6 +165,7 @@ temperature_symbol = pifacecad.LCDBitmap(
 memory_symbol = pifacecad.LCDBitmap(
 	[0xe, 0x1f, 0xe, 0x1f, 0xe, 0x1f, 0xe, 0x0])
 temp_symbol_index, memory_symbol_index = 0, 1
+
 
 ## sysinfo functions
 
@@ -193,6 +191,7 @@ def show_sysinfo():
 	global BACKLIGHT
 	global IS_RUNNING
 	IS_RUNNING = 0
+	
 	# init
 	cad.lcd.store_custom_bitmap(temp_symbol_index, temperature_symbol)
 	cad.lcd.store_custom_bitmap(memory_symbol_index, memory_symbol)
@@ -202,10 +201,8 @@ def show_sysinfo():
 	BACKLIGHT = True
 	cad.lcd.write("Waiting for IP..")
 	sysinfo_wait_ip()
-	#show_sysinfo()
 	
-	# display + update		## TODO : break while ?
-	#while True:
+	# display + update
 	cad.lcd.clear()
 	cad.lcd.write("{}\n".format(sysinfo_get_ip()))
 
@@ -214,7 +211,7 @@ def show_sysinfo():
 
 	cad.lcd.write_custom_bitmap(memory_symbol_index)
 	cad.lcd.write(":{}".format(sysinfo_get_free_mem()))
-	#sleep(UPDATE_INTERVAL)
+	
 	sleep(INFO_TIME)
 	display_default()
 
@@ -259,6 +256,5 @@ if __name__ == "__main__":
 				BACKLIGHT = False
 				cad.lcd.display_off()
 		
-		# DEBUG function
-		#sleep(10)
+		# shouldn't ever get here -> instead : atexit
 		stop_routine()
